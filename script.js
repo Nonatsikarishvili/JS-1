4d72915767d78ac05fec425075d6c36f060464b2
Nona <111157914+Nonatsikarishvili@users.noreply.github.com>
2023-02-21 21:28:01 -0500

    add html and js files

---
+++ script.js
@@ -0,0 +1,33 @@
+// davaleba #1
+let item = 100;
+ 
+if (item < 50){
+    console.log('naklebia 50-ze');
+}else if (item > 20){
+    console.log('metia 20-ze');
+}else{
+    console.log('shecdoma');
+}
+
+// davaleba #2
+
+let userName = 'mariam'
+
+let result = (userName == 'mariam') ? "true" : 'false';
+
+console.log(result);
+
+// davaleba #3
+
+let NameUser = 'mariami';
+
+switch (NameUser){
+    case "mariami":
+    console.log("true");
+    break;
+    case "nona":
+    console.log('name is nona');
+    break;
+    default:
+    console.log('false');
+}
